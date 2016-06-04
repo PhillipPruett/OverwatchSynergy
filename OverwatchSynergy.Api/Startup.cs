ee1adcb7c1961d72d1787e82b2311ac75c9cc286
Phillip Pruett <phpruett@microsoft.com>
2016-06-04 15:20:04 -0700

    Initial service setup

---
+++ OverwatchSynergy.Api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using OverwatchSynergy.Api;
+using Owin;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace OverwatchSynergy.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ OverwatchSynergy.Tests/RouteTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using NUnit.Framework;
+using OverwatchSynergy.Api;
+
+namespace OverwatchSynergy.Tests
+{
+    [TestFixture]
+    public class RouteTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            server = TestServer.Create<Startup>();
+            httpClient = server.HttpClient;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Dispose();
+        }
+
+        private TestServer server;
+        private HttpClient httpClient;
+
+        [Test]
+        public void get_heroes_returns_OK()
+        {
+            httpClient.GetAsync("/heroes").Result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void the_home_page_returned_OK()
+        {
+            httpClient.GetAsync("").Result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
