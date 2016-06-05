0ebba5bdb4d9c6fd7c851614278b321145376336
Evan Bailey <baileyevang@gmail.com>
2016-06-04 21:36:25 -0700

    send selected enemies to server for ranking

--- OverwatchSynergy.Api/wwwroot/overwatch-suggest.js
+++ OverwatchSynergy.Api/wwwroot/overwatch-suggest.js
@@ -1,8 +1,8 @@
-var HeroViewModel = function (hero) {
+var HeroViewModel = function (hero, calculatorViewModel) {
     this.Name = hero.Name;
 
     this.AddEnemy = function (enemy) {
-        console.log("adding " + JSON.stringify(enemy) + "as enemy");
+        calculatorViewModel.Enemies.push(enemy.Name);
     }
 
     this.AddAlly = function (ally) {
@@ -10,15 +10,28 @@ var HeroViewModel = function (hero) {
     }
 }
 
-var ViewModel = function (heroesJson) {
+var CalculatorViewModel = function (heroesJson) {
+    var calculatorViewModel = this;
+
     this.HeroViewModels = heroesJson.map(function (hero) {
-        return new HeroViewModel(hero);
+        return new HeroViewModel(hero, calculatorViewModel);
+    });
+    this.Enemies = ko.observableArray();
+    this.Enemies.subscribe(function (enemies) {
+        $.post({
+            url: "../calculator/GetHeroesStrengthAgainst",
+            data: JSON.stringify(enemies),
+            contentType: "application/json"
+        })
+        .done(function (data) {
+            console.log(JSON.stringify(data));
+        })
     });
 };
 
 $(document).ready(function () {
     $.getJSON("../heroes/")
         .done(function (data) {
-            ko.applyBindings(new ViewModel(data));
+            ko.applyBindings(new CalculatorViewModel(data));
         });
 });
