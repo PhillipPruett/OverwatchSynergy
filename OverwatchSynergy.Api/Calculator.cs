6e8291d027c85ce2b08059a0d8a7898e18093d5c
Evan Bailey <baileyevang@gmail.com>
2016-06-05 12:46:34 -0700

    Added OverallScore calculation, fixed tests I broke

--- OverwatchSynergy.Api/Calculator.cs
+++ OverwatchSynergy.Api/Calculator.cs
@@ -7,47 +7,47 @@ using OverwatchSynergy.Api.Heroes;
 namespace OverwatchSynergy.Api
 {
     public static class Calculator
-{
-    public static Hero[] Heroes = new[]
-                                   {
-                                       new Genji(),
-                                       new McCree(),
-                                       new Pharah(),
-                                       new Reaper(),
-                                       new Soldier76(),
-                                       new Tracer(),
-                                       new Bastion(),
-                                       new Hanzo(),
-                                       new Junkrat(),
-                                       new Mei(),
-                                       new Torbjorn(),
-                                       new Widowmaker(),
-                                       new DVa(),
-                                       new Reinhardt(),
-                                       new Roadhog(),
-                                       new Winston(),
-                                       new Zarya(),
-                                       new Lucio(),
-                                       new Mercy(),
-                                       new Symmetra(),
-                                       (Hero) new Zenyatta(),
-                                   };
+    {
+        public static Hero[] Heroes = new[]
+                                       {
+                                           new Genji(),
+                                           new McCree(),
+                                           new Pharah(),
+                                           new Reaper(),
+                                           new Soldier76(),
+                                           new Tracer(),
+                                           new Bastion(),
+                                           new Hanzo(),
+                                           new Junkrat(),
+                                           new Mei(),
+                                           new Torbjorn(),
+                                           new Widowmaker(),
+                                           new DVa(),
+                                           new Reinhardt(),
+                                           new Roadhog(),
+                                           new Winston(),
+                                           new Zarya(),
+                                           new Lucio(),
+                                           new Mercy(),
+                                           new Symmetra(),
+                                           (Hero) new Zenyatta(),
+                                       };
 
         public static IEnumerable<Weight> GetSynergies(IEnumerable<Hero> team)
         {
             return Heroes.Select(h => GetWeightForTeam(h, team));
         }
 
-    private static Weight GetWeightForTeam(Hero hero, IEnumerable<Hero> team)
-    {
-        return new Weight()
-               {
-                   Hero = hero,
-                   Value = team.Any()
-                               ? (int) team.Average(teamMate => teamMate.GetSynergyValue(hero))
-                               : 0
-               };
-    }
+        private static Weight GetWeightForTeam(Hero hero, IEnumerable<Hero> team)
+        {
+            return new Weight()
+                    {
+                        Hero = hero,
+                        Value = team.Any()
+                                    ? (int) team.Average(teamMate => teamMate.GetSynergyValue(hero))
+                                    : 0
+                    };
+        }
 
         public static IEnumerable<Weight> GetHeroesStrengthAgainst(IEnumerable<Hero> enemyTeam)
         {
@@ -60,11 +60,27 @@ namespace OverwatchSynergy.Api
             {
                 Hero = hero,
                 Value = enemyTeam.Any()
-                               ? (int)enemyTeam.Average(enemy => enemy.GetStrengthAgainstValue(hero))
+                                ? (int)enemyTeam.Average(enemy => enemy.GetStrengthAgainstValue(hero))
+                                : 0
+            };
+        }
+
+        public static IEnumerable<Weight> GetOverallScoresForAllHeroes(IEnumerable<Hero> enemyTeam, IEnumerable<Hero> team, double relativeSynergyWeight = 1)
+        {
+            return Heroes.Select(h => GetOverallScore(h, enemyTeam, team, relativeSynergyWeight));
+        }
+
+        private static Weight GetOverallScore(Hero hero, IEnumerable<Hero> enemyTeam, IEnumerable<Hero> team, double relativeSynergyWeight = 1)
+        {
+            return new Weight()
+            {
+                Hero = hero,
+                Value = enemyTeam.Any()
+                               ? (int)enemyTeam.Average(enemy => enemy.GetStrengthAgainstValue(hero)) + (int) (enemyTeam.Average(enemy => enemy.GetSynergyValue(hero)) * relativeSynergyWeight)
                                : 0
             };
         }
-}
+    }
 
     public class Weight
     {
---
+++ OverwatchSynergy.Api/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace OverwatchSynergy.Api
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet, Route("")]
+        public string Get()
+        {
+            return "Overwatch Synergy Calculator";
+        }
+    }
+}
