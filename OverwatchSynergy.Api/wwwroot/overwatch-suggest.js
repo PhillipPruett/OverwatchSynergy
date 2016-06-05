ab0f2beee1407fe4d0819d9470059bcd9a20cd79
Evan Bailey <baileyevang@gmail.com>
2016-06-05 02:20:04 -0700

    Basic functionality works

--- OverwatchSynergy.Api/wwwroot/overwatch-suggest.js
+++ OverwatchSynergy.Api/wwwroot/overwatch-suggest.js
@@ -1,12 +1,12 @@
 var HeroViewModel = function (hero, calculatorViewModel) {
     this.Name = hero.Name;
 
-    this.AddEnemy = function (enemy) {
+    this.AddAsEnemy = function (enemy) {
         calculatorViewModel.Enemies.push(enemy.Name);
     }
 
-    this.AddAlly = function (ally) {
-        console.log("adding " + JSON.stringify(ally) + "as ally");
+    this.AddAsAlly = function (ally) {
+        calculatorViewModel.Allies.push(ally.Name);
     }
 }
 
@@ -16,6 +16,30 @@ var CalculatorViewModel = function (heroesJson) {
     this.HeroViewModels = heroesJson.map(function (hero) {
         return new HeroViewModel(hero, calculatorViewModel);
     });
+
+    var _counterWeights = ko.observable([]);
+    var _synergyWeights = ko.observable([]);
+    var getWeightMap = function (weights) {
+        var map = {};
+        for (weight of weights) {
+            map[weight.Hero.Name] = weight.Value;
+        }
+        return map;
+    }
+
+    var combineWeights = function (counter, synergy) {
+        if (counter === undefined && synergy === undefined) {
+            return 50;
+        }
+        if (counter === undefined) {
+            return synergy;
+        }
+        if (synergy === undefined) {
+            return counter;
+        }
+        return (counter + synergy) / 2
+    }
+
     this.Enemies = ko.observableArray();
     this.Enemies.subscribe(function (enemies) {
         $.post({
@@ -24,8 +48,33 @@ var CalculatorViewModel = function (heroesJson) {
             contentType: "application/json"
         })
         .done(function (data) {
-            console.log(JSON.stringify(data));
+            _counterWeights(getWeightMap(data));
         })
+    });
+
+    this.Allies = ko.observableArray();
+    this.Allies.subscribe(function (allies) {
+        $.post({
+            url: "../calculator/GetHeroesThatHaveSynergiesWith",
+            data: JSON.stringify(allies),
+            contentType: "application/json"
+        })
+        .done(function (data) {
+            _synergyWeights(getWeightMap(data));
+        })
+    });
+
+    this.WeightedSuggestions = ko.computed(function () {
+        var counterWeights = ko.unwrap(_counterWeights),
+            synergyWeights = ko.unwrap(_synergyWeights),
+            allWeights = [];
+        for (hero of heroesJson) {
+            allWeights.push({
+                Name: hero.Name,
+                Weight: combineWeights(counterWeights[hero.Name], synergyWeights[hero.Name])
+            });
+        }
+        return allWeights.sort(function(a,b){return b.Weight - a.Weight;});
     });
 };
 
