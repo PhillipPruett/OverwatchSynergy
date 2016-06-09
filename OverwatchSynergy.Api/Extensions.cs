fc2bdbb8da16668437afe37395e0d3e4d2ffbb0b
Phillip Pruett <phpruett@microsoft.com>
2016-06-08 20:04:32 -0700

    suppress duplicate suggestions

--- OverwatchSynergy.Api/Extensions.cs
+++ OverwatchSynergy.Api/Extensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OverwatchSynergy.Api
 {
@@ -11,6 +12,25 @@ namespace OverwatchSynergy.Api
             {
                 action(item);
             }
+        }
+
+        public static IEnumerable<Weight> SuppressDuplicates(this IEnumerable<Weight> weights, IEnumerable<Hero> currentTeam)
+        {
+            //currentTeam.ForEach(h => weights.Single(w => w.Hero.Id == h.Id).Value = 0);
+
+            return weights.Select(w =>
+                           {
+                               if (currentTeam.Select(h => h.Id).Contains(w.Hero.Id))
+                               {
+                                   return new Weight()
+                                          {
+                                              Value = 0,
+                                              Hero = w.Hero
+                                          };
+                               }
+
+                               return w;
+                           });
         }
     }
 }
