ff3ba57a1182178454db678cd4ab2adf01ebc9fb
Evan Bailey <baileyevang@gmail.com>
2016-06-19 15:17:01 -0700

    Calculator controller doesn't dedupe team members, takes objective type as string

--- OverwatchSynergy.Api/CalculatorController.cs
+++ OverwatchSynergy.Api/CalculatorController.cs
@@ -10,9 +10,11 @@ namespace OverwatchSynergy.Api
         [HttpPost, Route("GetOverallScoresForAllHeroes")]
         public IEnumerable<Weight> GetOverallScoresForAllHeroes(GetOverallRankingsForAllHeroesRequest request)
         {
-            var scores = Calculator.GetOverallScoresForAllHeroes(Calculator.Heroes.Where(h => request.Opponents.Contains(h.Name)),
-                                                                 Calculator.Heroes.Where(h => request.Teammates.Contains(h.Name)),
-                                                                 request.ObjectiveGameType);
+            var enemyTeam = request.Opponents.Select(o => Calculator.Heroes.Single(h => h.Name == o));
+            var team = request.Teammates.Select(t => Calculator.Heroes.Single(h => h.Name == t));
+            var objectiveType = Calculator.ObjectiveTypes.Single(o => o.Id == request.ObjectiveGameType);
+
+            var scores = Calculator.GetOverallScoresForAllHeroes(enemyTeam, team, objectiveType);
             return scores;
         }
     }
@@ -21,6 +23,6 @@ namespace OverwatchSynergy.Api
     {
         public IEnumerable<string> Opponents { get; set; }
         public IEnumerable<string> Teammates { get; set; }
-        public ObjectiveType ObjectiveGameType { get; set; }
+        public string ObjectiveGameType { get; set; }
     }
 }
